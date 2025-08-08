14f45db3be8084bb09197f2c7034f0a92ea6187b
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-08 15:43:58 +0100

    Add pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import '../styles/globals.css'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        key={router.asPath}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{
+          duration: 0.3,
+          ease: [0.4, 0, 0.2, 1]
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  )
+}
+
+export default MyApp
